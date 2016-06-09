f6edc2ea8006076e59cd9198a2039673324a5cf2
Chenyuan Xie <1147719157xie@gmail.com>
2016-06-08 21:32:23 -0400

    Add project files.

---
+++ COMP123_S2016_Lesson4/Person.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_S2016_Lesson4
+{
+    /**
+     * This class defines a generic Person
+     * 
+     * @class Person
+     * @field {string} _name 
+     * @field {int} _age 
+     */
+    public class Person
+    {
+        // PRIVATE INSTANCE VARIABLES ++++++++++++++++++++++++++++++++++
+        private string _name;
+        private int _age;
+
+        // PUBLIC PROPERTIES ++++++++++++++++++++++++++++++++++++++++++
+
+        /**
+         * <summary>
+         * This is a public property for our private _name field
+         * </summary>
+         * 
+         * @property {string} Name
+         */
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+
+            set
+            {
+                this._name = value;
+            }
+        }
+
+        /**
+         * <summary>
+         * This is a public property for our private _age field
+         * </summary>
+         * 
+         * @property {int} Age
+         */
+        public int Age
+        {
+            get
+            {
+                return this._age;
+            }
+
+            set
+            {
+                this._age = value;
+            }
+        }
+
+
+        // CONSTRUCTORS +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        /**
+         * <summary>
+         * This is the default empty constructor for the Person class
+         * </summary>
+         * 
+         * @constructor Person
+         */
+        public Person()
+        {
+            // Initialize public properties
+            this.Name = "unknown name!";
+            this.Age = 0;
+        }
+
+        /**
+         * <summary>
+         * This is a consructor that takes name as a parameter and passes 
+         * it to the _name private instance variable
+         * </summary>
+         * 
+         * @constructor Person
+         * @param {string} name
+         */
+        public Person(string name)
+        {
+            this.Name = name;
+            this.Age = 0;
+        }
+
+        /**
+         * <summary>
+         * This is a constructor that takes age as a parameter and passes
+         * it to the _age private isntance variable
+         * </summary>
+         * 
+         * @constructor Person
+         * @param {int} age
+         */
+        public Person(int age)
+        {
+            this.Name = "unknown name!";
+            this.Age = age;
+        }
+
+        /** <summary>
+         * This is a constructor that takes both name and age as parameters.
+         * It assigns the local name value to the Name property.
+         * It assigns the local age value to the age property.
+         * </summary>
+         * 
+         * @constructor Person
+         * @param {string} name
+         * @param {int} age
+         */
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        // PUBLIC METHODS ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        /**
+         * <summary>
+         * This method outputs the _name value with " says hello!" to the console.
+         * </summary>
+         * 
+         * @method SaysHello
+         * @returns {void}
+         */
+        public void SaysHello()
+        {
+            Console.WriteLine(this.Name + " says hello!");
+        }
+
+        /**
+         * <summary>
+         * This method outputs the _name and _age values in the following
+         * format: _name + " is " + _age + " years old".
+         * </summary>
+         * 
+         * @method ShowAge
+         * @returns {void}
+         */
+        public void ShowAge()
+        {
+            Console.WriteLine(this.Name + " is " + this.Age + " years old.");
+        }
+    }
+}
---
+++ COMP123_S2016_Lesson4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_S2016_Lesson4
+{
+    /**
+     * This class is the "driver" class for our Program
+     * 
+     * @class Program
+     */
+    public class Program
+    {
+        /**
+         * the main method for our driver class Program
+         * 
+         * @method Main
+         * @param {sting[]} args
+         */
+        public static void Main(string[] args)
+        {
+            // create a new instance of the Person class
+            Person tanya = new Person();
+            tanya.Name = "Tanya";
+            tanya.Age = 28;
+            tanya.SaysHello();
+            tanya.ShowAge();
+
+            Console.WriteLine();
+
+            Person tom = new Person("Tom");
+            tom.Age = 47;
+            tom.SaysHello();
+            tom.ShowAge();
+
+            Console.WriteLine();
+
+            Person mark = new Person(30);
+            mark.Name = "Mark";
+            mark.SaysHello();
+            mark.ShowAge();
+
+            Console.WriteLine();
+
+            Person mary = new Person("Mary", 23);
+            mary.SaysHello();
+            mary.ShowAge();
+
+
+        }
+    }
+
+}
