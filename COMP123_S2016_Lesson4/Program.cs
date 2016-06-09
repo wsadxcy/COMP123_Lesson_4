cad087f95e4e7982d6b924004b8448ad5f7772ab
Chenyuan Xie <1147719157xie@gmail.com>
2016-06-08 23:45:45 -0400

    initial commit

--- COMP123_S2016_Lesson4/Program.cs
+++ COMP123_S2016_Lesson4/Program.cs
@@ -21,33 +21,11 @@ namespace COMP123_S2016_Lesson4
          */
         public static void Main(string[] args)
         {
-            // create a new instance of the Person class
-            Person tanya = new Person();
-            tanya.Name = "Tanya";
-            tanya.Age = 28;
-            tanya.SaysHello();
-            tanya.ShowAge();
-
-            Console.WriteLine();
-
-            Person tom = new Person("Tom");
-            tom.Age = 47;
-            tom.SaysHello();
-            tom.ShowAge();
-
-            Console.WriteLine();
-
-            Person mark = new Person(30);
-            mark.Name = "Mark";
-            mark.SaysHello();
-            mark.ShowAge();
-
-            Console.WriteLine();
-
-            Person mary = new Person("Mary", 23);
-            mary.SaysHello();
-            mary.ShowAge();
-
+            // create a new instance of the Student class
+            Student Chen = new Student();
+            Chen.Age = 22;
+            Chen.Name = "Chen";
+            Chen.SaysHello();
 
         }
     }
---
+++ COMP123_S2016_Lesson4/Student.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_S2016_Lesson4
+{
+    /**
+     * <summary>
+     * This is the Student class. It is derived from the Person class
+     * </summary>
+     * 
+     * @class Student
+     * @extends Person
+     */
+    class Student : Person
+    {
+        private string _studentID;
+
+
+        /**
+         * <summary>
+         * This property, StudentID, includes a getter and setter for the private
+         * instance variable _studentID
+         * </summary>
+         * 
+         * @property StudentID
+         */
+         public string StudentID
+        {
+            get
+            {
+                return this._studentID;
+            }
+
+            set
+            {
+                this._studentID = value;
+            }
+        }
+            /**
+         * <summary>
+         * This constructor takes three parameters: name, age and studentID
+         * </summary>
+         * 
+         * @constructor Student
+         * @extends Person
+         * @param {string} name
+         * @param {int} age
+         * @param {string} studentID
+         */
+        public Student(string name, int age, string studentID)
+            : base(name, age)
+        {
+            this.StudentID = studentID;
+
+        }
+
+        /**
+         * <summary>
+         * This method enables the Studies behaviour for the student
+         * </summary>
+         * 
+         * @method Studies
+         * @returns {void}
+         */
+        public void Studies()
+        {
+            Console.WriteLine(this.Name + " studies.");
+        }
+    }
+}
